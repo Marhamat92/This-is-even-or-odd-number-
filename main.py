86bdaa0e095cd89ea10be8cf330b584c7716750f
Marhamat <marhamatbagirov@gmail.com>
2020-12-28 19:15:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if number % 2==1:
+  print("This is odd number")
+else:
+  print("This is even number")
+
+
+
+
